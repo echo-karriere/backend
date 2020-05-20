3f0e8d00eab6a82ae381a3f764e2585eb2a946f2
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-20 21:43:30 +0200

    Add filtering, edges to pages GraphQL endpoint

--- apps/pages/schema.py
+++ apps/pages/schema.py
@@ -1,28 +1,17 @@
-import graphene
+from graphene import Node
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
+
 from .models import Page
 
 
-class PageType(DjangoObjectType):
+class PageNode(DjangoObjectType):
     class Meta:
         model = Page
+        interfaces = (Node,)
+        filter_fields = {"title": ["exact", "icontains", "istartswith"]}
 
 
 class Query(object):
-    page = graphene.Field(PageType, id=graphene.Int(), title=graphene.String())
-    all_pages = graphene.List(PageType)
-
-    def resolve_all_pages(self, info, **kwargs):
-        return Page.objects.all()
-
-    def resolve_page(self, info, **kwargs):
-        id = kwargs.get("id")
-        title = kwargs.get("title")
-
-        if id is not None:
-            return Page.objects.get(pk=id)
-
-        if title is not None:
-            return Page.objects.get(title=title)
-
-        return None
+    page = Node.Field(PageNode)
+    all_pages = DjangoFilterConnectionField(PageNode)
