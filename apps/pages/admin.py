dec97d057d3071416ef73d67d622ad0b7024d3c3
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-20 22:27:51 +0200

    Add date fields to pages, filtering to admin

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -4,7 +4,8 @@ from .models import Page, Namespace
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ("title", "status", "slug")
+    list_display = ("title", "status", "published_on", "created_on", "slug")
+    list_filter = ["published_on", "status", "modified_on"]
 
 
 @admin.register(Namespace)
--- apps/pages/models.py
+++ apps/pages/models.py
@@ -1,5 +1,6 @@
 from ckeditor.fields import RichTextField
 from django.db import models
+from django.utils import timezone
 from django.utils.text import slugify
 
 
@@ -24,13 +25,19 @@ class Page(models.Model):
     DELETED = "X"
     STATUS_CHOICES = [(PUBLISHED, "Published"), (DRAFT, "Draft"), (DELETED, "Deleted")]
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     content = RichTextField()
     slug = models.SlugField(editable=False)
     namespace = models.ForeignKey(
         Namespace, on_delete=models.CASCADE, blank=True, null=True
     )
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=DRAFT)
+    created_on = models.DateTimeField(editable=False, auto_now_add=True)
+    modified_on = models.DateTimeField(editable=False, auto_now=True)
+    published_on = models.DateTimeField(editable=False)
+
+    class Meta:
+        ordering = ["modified_on"]
 
     def __str__(self):
         return self.title
@@ -44,5 +51,8 @@ class Page(models.Model):
             self.slug = slug
         else:
             self.slug = f"/{slugify(self.title)}/"
+
+        if self.status == Page.PUBLISHED:
+            self.published_on = timezone.now()
 
         super(Page, self).save(*args, **kwargs)
