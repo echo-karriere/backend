54b828ef4f9de0563c5349b8641b86f4127549f5
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-27 20:15:04 +0200

    Hello, Kotlin!

---
+++ test/ApplicationTest.kt
@@ -0,0 +1,23 @@
+package no.echokarriere
+
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
