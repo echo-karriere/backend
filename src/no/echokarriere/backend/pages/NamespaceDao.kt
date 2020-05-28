0d51145c096056dd187a7cd8fe58de01f2a8d158
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-05-28 20:22:45 +0200

    Fully flesh out API for namespace

--- src/no/echokarriere/backend/pages/NamespaceDao.kt
+++ src/no/echokarriere/backend/pages/NamespaceDao.kt
@@ -1,5 +1,6 @@
 package no.echokarriere.backend.pages
 
+import org.jdbi.v3.sqlobject.SingleValue
 import org.jdbi.v3.sqlobject.customizer.BindBean
 import org.jdbi.v3.sqlobject.statement.SqlQuery
 import org.jdbi.v3.sqlobject.statement.SqlUpdate
@@ -10,22 +11,38 @@ interface NamespaceDao {
         CREATE TABLE IF NOT EXISTS namespace
             (
                 id          serial  NOT NULL PRIMARY KEY,
-                title       text    NOT NULL,
+                title       text    NOT NULL UNIQUE,
                 description text    NOT NULL,
-                namespace   text    NOT NULL
+                namespace   text    NOT NULL UNIQUE
             )
-    """
+        """
     )
     fun createTable()
+
+    @SqlQuery("SELECT * FROM namespace")
+    fun selectAll(): List<Namespace>
+
+    @SqlQuery("SELECT * FROM namespace WHERE id = :id")
+    @SingleValue
+    fun selectOne(id: Int): Namespace?
 
     @SqlUpdate(
         """
         INSERT INTO namespace(title, description, namespace)
         VALUES (:title, :description, :namespace)
-    """
+        """
     )
     fun insert(@BindBean namespace: Namespace)
 
-    @SqlQuery("SELECT * FROM namespace")
-    fun selectAll(): List<Namespace>
+    @SqlUpdate(
+        """
+        UPDATE namespace
+        SET title = :title, description = :description, namespace = :namespace
+        WHERE id = :id
+    """
+    )
+    fun update(id: Int, @BindBean namespace: Namespace)
+
+    @SqlUpdate("DELETE FROM namespace WHERE ID = :id")
+    fun delete(id: Int)
 }
