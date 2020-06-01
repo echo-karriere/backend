9cf254f65bcac17b3e00e4fbe3164fa9cb7a082d
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-01 17:23:11 +0200

    Make contact form into a full REST API

--- apps/forms/contact/views.py
+++ apps/forms/contact/views.py
@@ -1,29 +1,21 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import mixins, status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .models import ContactSchema
 from .serializer import ContactSchemaSerializer
 
 
-@method_decorator(
-    name="create",
-    decorator=swagger_auto_schema(
-        operation_summary="Send a contact us message", responses={201: "OK", 400: "Bad request"}
-    ),
-)
-class ContactViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+@method_decorator(name="list", decorator=swagger_auto_schema(operation_summary="Get all contact requests"))
+@method_decorator(name="retrieve", decorator=swagger_auto_schema(operation_summary="Get a single contact request"))
+@method_decorator(name="update", decorator=swagger_auto_schema(operation_summary="Update a request form"))
+@method_decorator(name="partial_update", decorator=swagger_auto_schema(operation_summary="Partially update a request"))
+@method_decorator(name="destroy", decorator=swagger_auto_schema(operation_summary="Delete a request"))
+@method_decorator(name="create", decorator=swagger_auto_schema(operation_summary="Send a contact us message"))
+class ContactViewSet(viewsets.ModelViewSet):
     """
     API endpoint for the contact us form on our website.
     """
 
     queryset = ContactSchema.objects.all()
     serializer_class = ContactSchemaSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
