f450776a5c7bc69d9d6809c01fd0fd7babcfad46
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-16 22:05:27 +0200

    Update packages, list users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly prismaService: PrismaService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("users")
+  async getUsers(): Promise<unknown[]> {
+    return this.prismaService.user.findMany();
   }
 }
