5b9b6bfc60d9b7023bc8323f5c2be1ddf43c886f
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-28 15:47:12 +0200

    Move back to Express, set JWT tokens in cookies

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,16 +3,17 @@ import { passwordDTO, loginDTO } from "./dto";
 import argon2, { argon2id } from "argon2";
 import { User } from "../user/models/user.model";
 import { Mutation, Args, Resolver } from "@nestjs/graphql";
-import { JwtToken } from "./models/jwt.model";
 import { AuthService } from "./auth.service";
 import { PrismaService } from "../prisma/prisma.service";
+import { GqlRes } from "./auth.decorators";
+import { Response } from "express";
 
 @Resolver(() => User)
 export class AuthResolver {
   constructor(private readonly authService: AuthService, private readonly prismaService: PrismaService) {}
 
-  @Mutation((returns) => JwtToken)
-  async login(@Args() args: loginDTO): Promise<{ accessToken: string; refreshToken: string } | UnauthorizedException> {
+  @Mutation((returns) => User)
+  async login(@Args() args: loginDTO, @GqlRes() res: Response): Promise<User | UnauthorizedException> {
     const user = await this.prismaService.user.findOne({
       where: { email: args.email },
     });
@@ -21,9 +22,16 @@ export class AuthResolver {
 
     try {
       if (await argon2.verify(user.password, args.password)) {
-        return this.authService.generateToken({ id: user.id, email: user.email });
-      } else return new UnauthorizedException(`Unable to login`);
+        const { accessToken, refreshToken } = await this.authService.generateToken({ id: user.id, email: user.email });
+        res.cookie("access_token", accessToken, { httpOnly: true });
+        res.cookie("refresh_token", refreshToken, { httpOnly: true });
+
+        return user;
+      } else {
+        return new UnauthorizedException(`Unable to login`);
+      }
     } catch (err) {
+      console.error(err);
       throw new InternalServerErrorException(`Internal server error occurred`);
     }
   }
--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -5,6 +5,7 @@ import { join } from "path";
 export default registerAs(
   "graphql",
   (): GqlModuleOptions => ({
+    context: ({ req, res }: { req: unknown; res: unknown }) => ({ req, res }),
     debug: process.env.NODE_ENV === "development",
     playground: process.env.NODE_ENV === "development",
     introspection: process.env.NODE_ENV === "development",
