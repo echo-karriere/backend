d982ed0284b4095129df9840aa50f3d823f7b969
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-28 15:47:12 +0200

    Move back to Express, set JWT tokens in cookies

--- src/auth/auth.decorators.ts
+++ src/auth/auth.decorators.ts
@@ -1,11 +1,9 @@
+import { Response } from "express";
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { GqlExecutionContext } from "@nestjs/graphql";
 
-export const GraphqlResponse = createParamDecorator(
-  (data: unknown, context: ExecutionContext): Response => {
-    const ctx = GqlExecutionContext.create(context);
-
+export const GqlRes = createParamDecorator(
+  (data: unknown, context: ExecutionContext): Response =>
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-    return ctx.getContext().res;
-  },
+    GqlExecutionContext.create(context).getContext().res,
 );
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from "@nestjs/core";
-import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT") ?? 3000;
   await app.listen(port, "0.0.0.0");
 
-  console.log(`env: ${process.env.NODE_ENV ?? ""}`);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
