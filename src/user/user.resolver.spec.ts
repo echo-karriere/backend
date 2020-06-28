b9c988202a2583c38b524b469e4d5a2cbce50e07
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-06-28 14:43:56 +0200

    Fix tests, add auth.config provider

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserResolver } from "./user.resolver";
-import { PrismaService } from "../prisma/prisma.service";
+import { AuthModule } from "../auth/auth.module";
+import { PrismaModule } from "../prisma/prisma.module";
 
 describe("UserResolver", () => {
   let resolver: UserResolver;
 
   beforeEach(async () => {
+    process.env.JWT_TOKEN = "iamtesting";
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver, PrismaService],
+      imports: [PrismaModule, AuthModule],
+      providers: [UserResolver],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
