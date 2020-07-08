2468217ca2bf3d96973fb38f636faf5b72d35a49
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-08 15:58:21 +0200

    Add passport local auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,11 @@ import { PassportModule } from "@nestjs/passport";
 import { PrismaModule } from "../prisma/prisma.module";
 import { AuthResolver } from "./auth.resolver";
 import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./local.strategy";
 
 @Module({
-  imports: [
-    PrismaModule,
-    PassportModule.register({
-      defaultStrategy: "jwt",
-    }),
-    ConfigModule,
-  ],
-  providers: [AuthService, AuthResolver],
+  imports: [PrismaModule, PassportModule, ConfigModule],
+  providers: [AuthService, AuthResolver, LocalStrategy],
   exports: [AuthService, AuthResolver],
 })
 export class AuthModule {}
