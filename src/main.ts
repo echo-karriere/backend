4182ba77f062459d25be089e237fe4d11427fa03
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-08 13:31:23 +0200

    Actually move to Express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ConfigService } from "@nestjs/config";
-import { NestFastifyApplication, FastifyAdapter } from "@nestjs/platform-fastify";
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT") ?? 3000;
   await app.listen(port, "0.0.0.0");
