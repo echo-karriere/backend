3c5f6c1b80a0e055af2bf6d0c3030b7f3c1e2760
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-10 00:09:19 +0200

    Rename Namespace to NamespaceEntity

--- src/main/kotlin/no/echokarriere/backend/namespace/NamespaceEntity.kt
+++ src/main/kotlin/no/echokarriere/backend/namespace/NamespaceEntity.kt
@@ -3,7 +3,7 @@ package no.echokarriere.backend.namespace
 import org.jdbi.v3.core.mapper.reflect.ColumnName
 import java.util.UUID
 
-data class Namespace(
+data class NamespaceEntity(
     @ColumnName("id")
     val id: UUID = UUID.randomUUID(),
     @ColumnName("title")
