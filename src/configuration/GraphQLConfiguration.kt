470b0205adf5b3748e3534f5f9dc57f6040cdae5
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-15 18:52:01 +0200

    Configure GraphQL for Ktor

--- src/configuration/GraphQLConfiguration.kt
+++ src/configuration/GraphQLConfiguration.kt
@@ -1,22 +1,72 @@
 package no.echokarriere.backend.configuration
 
+import com.expediagroup.graphql.SchemaGeneratorConfig
+import com.expediagroup.graphql.TopLevelObject
+import com.expediagroup.graphql.hooks.SchemaGeneratorHooks
+import com.expediagroup.graphql.toSchema
+import graphql.ExecutionInput
+import graphql.GraphQL
 import graphql.language.StringValue
 import graphql.scalars.ExtendedScalars
 import graphql.schema.Coercing
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.http.content.defaultResource
+import io.ktor.http.content.static
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.post
+import io.ktor.routing.routing
 import java.time.OffsetDateTime
 import java.util.UUID
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
+import no.echokarriere.namespace.NamespaceMutationResolver
+import no.echokarriere.namespace.NamespaceQueryResolver
+import no.echokarriere.namespace.NamespaceRepository
 
-class GraphQLConfiguration {
-    class CustomSchemaGeneratorHooks {
-        fun willGenerateGraphQLType(type: KType): GraphQLType? = when (type.classifier as? KClass<*>) {
-            UUID::class -> graphQLUuidType
-            OffsetDateTime::class -> graphqlDateTimeType
-            else -> null
+data class GraphQLRequest(val query: String, val operationName: String?, val variables: Map<String, Any>?)
+
+fun Application.installGraphQL(namespaceRepository: NamespaceRepository) {
+    val config =
+        SchemaGeneratorConfig(supportedPackages = listOf("no.echokarriere"), hooks = CustomSchemaGeneratorHooks())
+    val queries = listOf(TopLevelObject(NamespaceQueryResolver(namespaceRepository)))
+    val mutations = listOf(TopLevelObject(NamespaceMutationResolver(namespaceRepository)))
+
+    val schema = toSchema(config, queries, mutations)
+    val graphql = GraphQL.newGraphQL(schema).build()
+
+    suspend fun ApplicationCall.executeQuery() {
+        val request = receive<GraphQLRequest>()
+        val executionInput = ExecutionInput.newExecutionInput()
+            // .context(ApplicationCallContext(this))
+            .query(request.query)
+            .operationName(request.operationName)
+            .variables(request.variables)
+            .build()
+
+        respond(graphql.execute(executionInput))
+    }
+
+    routing {
+        post("/graphql") {
+            call.executeQuery()
         }
+
+        static("playground") {
+            defaultResource("static/playground.html")
+        }
+    }
+}
+
+class CustomSchemaGeneratorHooks : SchemaGeneratorHooks {
+    override fun willGenerateGraphQLType(type: KType): GraphQLType? = when (type.classifier as? KClass<*>) {
+        UUID::class -> graphQLUuidType
+        OffsetDateTime::class -> graphqlDateTimeType
+        else -> null
     }
 }
 
--- src/namespace/NamespaceRepository.kt
+++ src/namespace/NamespaceRepository.kt
@@ -1,13 +1,13 @@
 package no.echokarriere.namespace
 
-interface NamespaceRepository {
-    fun selectAll(): List<NamespaceEntity>
+class NamespaceRepository {
+    fun selectAll(): List<NamespaceEntity> = TODO()
 
-    fun selectOne(id: Int): NamespaceEntity?
+    fun selectOne(id: Int): NamespaceEntity? = TODO()
 
-    fun insert(namespace: NamespaceEntity): NamespaceEntity
+    fun insert(namespace: NamespaceEntity): NamespaceEntity = TODO()
 
-    fun update(id: Int, namespace: NamespaceEntity): NamespaceEntity?
+    fun update(id: Int, namespace: NamespaceEntity): NamespaceEntity? = TODO()
 
-    fun delete(id: Int): Int
+    fun delete(id: Int): Int = TODO()
 }
