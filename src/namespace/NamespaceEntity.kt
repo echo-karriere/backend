b191042432ceeb88146cc985cdf4effeccb13cc4
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-16 22:19:34 +0200

    And implement Exposed table for namespace, add to resolver

--- src/namespace/NamespaceEntity.kt
+++ src/namespace/NamespaceEntity.kt
@@ -1,6 +1,7 @@
 package no.echokarriere.namespace
 
 import java.util.UUID
+import org.jetbrains.exposed.sql.Table
 
 data class NamespaceEntity(
     val id: UUID = UUID.randomUUID(),
@@ -8,3 +9,11 @@ data class NamespaceEntity(
     val description: String,
     val namespace: String
 )
+
+object Namespaces : Table("namespace") {
+    val id = uuid("id")
+    val title = text("title")
+    val description = text("description")
+    val namespace = text("namespace")
+    override val primaryKey = PrimaryKey(id)
+}
