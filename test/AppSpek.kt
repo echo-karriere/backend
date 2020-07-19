ea74fa3a3c177d5b44eca4182a48f0da90b23828
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-07-19 19:16:14 +0200

    Run Flyway migrations during tests

--- test/AppSpek.kt
+++ test/AppSpek.kt
@@ -4,6 +4,7 @@ import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.withTestApplication
 import kotlinx.coroutines.runBlocking
 import no.echokarriere.configuration.DatabaseConfig
+import org.flywaydb.core.Flyway
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.dsl.Root
 import org.testcontainers.containers.PostgreSQLContainer
@@ -13,12 +14,17 @@ abstract class AppSpek(val appRoot: Root.() -> Unit) : Spek({
     beforeGroup {
         if (!postgresContainer.isRunning) {
             postgresContainer.start()
+            flyway.migrate()
         }
     }
 }) {
     companion object {
         class AppPostgreSQLContainer : PostgreSQLContainer<AppPostgreSQLContainer>(), DatabaseConfig
         val postgresContainer = AppPostgreSQLContainer()
+        val flyway: Flyway = Flyway
+            .configure()
+            .dataSource(postgresContainer.jdbcUrl, postgresContainer.username, postgresContainer.password)
+            .load()
 
         fun <R> withApp(test: suspend TestApplicationEngine.() -> R) = withTestApplication({
             module(testing = true, database = postgresContainer)
