903806f7934225015df0ee651c9be838c5c8869a
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-17 22:50:45 +0200

    Add complete test coverage to category resolvers

--- test/category/CategoryResolverSpec.kt
+++ test/category/CategoryResolverSpec.kt
@@ -14,8 +14,11 @@ import io.ktor.server.testing.withTestApplication
 import io.restassured.path.json.JsonPath
 import no.echokarriere.module
 import no.echokarriere.utils.DatabaseDescribeSpec
+import java.util.UUID
 
 class CategoryResolverSpec : DatabaseDescribeSpec({
+    lateinit var id: UUID
+
     describe("CategoryResolver") {
         it("can create a new category") {
             withTestApplication(Application::module) {
@@ -31,10 +34,54 @@ class CategoryResolverSpec : DatabaseDescribeSpec({
                 call.response shouldHaveStatus HttpStatusCode.OK
 
                 val json = JsonPath(call.response.content!!).setRootPath("data.createCategory")
-                json.getString("id") shouldNotBe null
+                val localId = json.getString("id")
+                localId shouldNotBe null
+                id = UUID.fromString(localId)
                 json.getString("title") shouldBe title
                 json.getString("description") shouldBe description
                 json.getString("slug") shouldBe slug
+            }
+        }
+
+        it("can update an existing category") {
+            withTestApplication(Application::module) {
+                val title = "Updated category"
+                val description = "New description"
+                val slug = "updated-category"
+
+                val update = handleRequest(HttpMethod.Post, "/graphql") {
+                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody("{\"query\":\"mutation UpdateCategory {\\n  updateCategory(input: {id: \\\"$id\\\", category: {title: \\\"$title\\\", description: \\\"$description\\\", slug: \\\"$slug\\\"}}) {\\n    title\\n    description\\n    slug\\n  }\\n}\\n\",\"variables\":null,\"operationName\":\"UpdateCategory\"}")
+                }
+
+                val json = JsonPath(update.response.content!!).setRootPath("data.updateCategory")
+                json.getString("title") shouldBe title
+                json.getString("description") shouldBe description
+                json.getString("slug") shouldBe slug
+            }
+        }
+
+        it("can delete a category") {
+            withTestApplication(Application::module) {
+                val delete = handleRequest(HttpMethod.Post, "/graphql") {
+                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody("{\"query\":\"mutation DeleteCategory {\\n  deleteCategory(id:\\\"$id\\\")\\n}\\n\",\"variables\":null,\"operationName\":\"DeleteCategory\"}")
+                }
+
+                val json = JsonPath(delete.response.content!!).setRootPath("data.deleteCategory")
+                json.getBoolean("") shouldBe true
+            }
+        }
+
+        it("cannot delete a previously deleted category") {
+            withTestApplication(Application::module) {
+                val delete = handleRequest(HttpMethod.Post, "/graphql") {
+                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody("{\"query\":\"mutation DeleteCategory {\\n  deleteCategory(id:\\\"$id\\\")\\n}\\n\",\"variables\":null,\"operationName\":\"DeleteCategory\"}")
+                }
+
+                val json = JsonPath(delete.response.content!!).setRootPath("data.deleteCategory")
+                json.getBoolean("") shouldBe false
             }
         }
     }
