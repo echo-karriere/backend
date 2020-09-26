3022e46dddddfc12c9d09bfb821d655e8b636ca4
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-09-26 19:36:30 +0200

    Create utility methods to create and login an admin user

--- test/utils/Utils.kt
+++ test/utils/Utils.kt
@@ -1,15 +1,28 @@
 package no.echokarriere.utils
 
 import io.ktor.http.ContentType
+import io.ktor.http.Cookie
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.server.testing.TestApplicationCall
 import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.TestApplicationResponse
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 
-fun TestApplicationEngine.graphqlQuery(request: String): TestApplicationCall =
+private fun buildCookie(cookie: Cookie): String = "${cookie.name}=${cookie.value}; "
+
+fun TestApplicationEngine.graphqlQuery(request: String, authCookie: Cookie? = null): TestApplicationCall =
     this.handleRequest(HttpMethod.Post, "/graphql") {
         addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+        authCookie?.let { addHeader("Cookie", buildCookie(authCookie)) }
         setBody(request)
     }
+
+fun TestApplicationEngine.createAdminUserAndLogin(): TestApplicationResponse {
+    graphqlQuery("{\"query\":\"mutation CreateUser {\\n  createUser(input: {email: \\\"admin@example.org\\\", name: \\\"Test User\\\", password: \\\"password123\\\", type: ADMIN}) {\\n\\t\\tactive\\n    email\\n    id\\n    name\\n    type\\n  }\\n}\\n\",\"variables\":null,\"operationName\":\"CreateUser\"}")
+    val loginQuery =
+        graphqlQuery("{\"query\":\"mutation Login {\\n  login(input: {email: \\\"admin@example.org\\\", password: \\\"password123\\\"}) {\\n    token\\n  }\\n}\\n\",\"variables\":null,\"operationName\":\"Login\"}")
+
+    return loginQuery.response
+}
