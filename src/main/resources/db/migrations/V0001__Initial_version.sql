b0f576b7246e9e271e589f38aa1ef4cb8e9f791b
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-10-04 18:40:10 +0200

    Add tables for forms and form submissions

--- src/main/resources/db/migrations/V0001__Initial_version.sql
+++ src/main/resources/db/migrations/V0001__Initial_version.sql
@@ -31,4 +31,23 @@ CREATE TABLE IF NOT EXISTS refresh_token
     expires_at    timestamptz NOT NULL,
     created_at    timestamptz NOT NULL DEFAULT now(),
     FOREIGN KEY (user_id) REFERENCES "user" (id) ON UPDATE RESTRICT ON DELETE CASCADE
-)
+);
+
+CREATE TABLE IF NOT EXISTS form
+(
+    id          uuid        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    title       text        NOT NULL UNIQUE,
+    description text        NOT NULL,
+    created_at  timestamptz NOT NULL             DEFAULT now(),
+    modified_at timestamptz
+);
+
+CREATE TABLE IF NOT EXISTS form_submission
+(
+    id           uuid        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    form         uuid        NOT NULL,
+    data         jsonb       NOT NULL,
+    submitted_at timestamptz NOT NULL             DEFAULT now(),
+    modified_at  timestamptz,
+    FOREIGN KEY (form) REFERENCES form ON DELETE CASCADE
+);
