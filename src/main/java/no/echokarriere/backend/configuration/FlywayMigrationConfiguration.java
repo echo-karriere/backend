be439710ff1694648e5e7d8e4cb5ea52830f8db3
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-13 14:29:50 +0100

    Fix tests for application

--- src/main/java/no/echokarriere/backend/configuration/FlywayMigrationConfiguration.java
+++ src/main/java/no/echokarriere/backend/configuration/FlywayMigrationConfiguration.java
@@ -3,11 +3,13 @@ package no.echokarriere.backend.configuration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class FlywayMigrationConfiguration {
 
     @Bean
+    @Primary
     public FlywayMigrationStrategy flywayMigrationStrategy() {
         return flyway -> {
             // If we are in a development environment, clean out the database on
--- src/test/java/no/echokarriere/backend/FlywayMigrationConfig.java
+++ src/test/java/no/echokarriere/backend/FlywayMigrationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @TestConfiguration
 @Profile("test")
