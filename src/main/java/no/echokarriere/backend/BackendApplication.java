d840d45ef136b1c4b25a291fe306b7fb425d88b6
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-13 16:53:57 +0100

    Configure Springdoc

--- src/main/java/no/echokarriere/backend/BackendApplication.java
+++ src/main/java/no/echokarriere/backend/BackendApplication.java
@@ -1,13 +1,28 @@
 package no.echokarriere.backend;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("echo karriere API")
+                        .description("API used in echo karriere to manage our events")
+                        .termsOfService("http://www.echokarriere.no")
+                        .license(new License().name("MIT").url("https://github.com/echo-karriere/backend/blob/master/LICENSE"))
+                );
+    }
 }
