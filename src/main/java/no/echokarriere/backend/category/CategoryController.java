bae2b8e4265a09c5c0d0369efce8030ec24cd89a
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-14 15:55:44 +0100

    Add OpenAPI documentation to CategoryController

--- src/main/java/no/echokarriere/backend/category/CategoryController.java
+++ src/main/java/no/echokarriere/backend/category/CategoryController.java
@@ -1,5 +1,11 @@
 package no.echokarriere.backend.category;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import no.echokarriere.backend.category.dto.CreateCategoryDTO;
 import no.echokarriere.backend.category.dto.UpdateCategoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,27 +26,60 @@ public class CategoryController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all categories", tags = {"categories"})
     public List<Category> all() {
         return categoryService.all();
     }
 
     @GetMapping("{id}")
-    public Category one(@PathVariable UUID id) {
+    @Operation(summary = "Get information about a single category", tags = {"categories"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Category not found", content = @Content)
+    })
+    public Category one(
+            @Parameter(description = "ID of category") @PathVariable UUID id
+    ) {
         return categoryService.one(id);
     }
 
     @PostMapping
-    public Category create(@RequestBody CreateCategoryDTO categoryDTO) {
+    @Operation(summary = "Create a new category", tags = {"categories"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "400", description = "Could not create a new category", content = @Content)
+    })
+    public Category create(
+            @Parameter(description = "Data used to create a new category") @RequestBody CreateCategoryDTO categoryDTO
+    ) {
         return categoryService.create(categoryDTO);
     }
 
     @PutMapping("{id}")
-    public Category update(@RequestBody UpdateCategoryDTO categoryDTO, @PathVariable UUID id) {
+    @Operation(summary = "Update an existing category", tags = {"categories"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Category.class))),
+            @ApiResponse(responseCode = "400", description = "Could not update category, bad request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Could not find category", content = @Content)
+    })
+    public Category update(
+            @Parameter(description = "Data used to update category") @RequestBody UpdateCategoryDTO categoryDTO,
+            @Parameter(description = "ID of category") @PathVariable UUID id
+    ) {
         return categoryService.update(categoryDTO, id);
     }
 
     @DeleteMapping("{id}")
-    public boolean delete(@PathVariable UUID id) {
+    @Operation(summary = "Delete a category from the service", tags = {"categories"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Boolean.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid ID supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Category not found", content = @Content)
+    })
+    public boolean delete(
+            @Parameter(description = "ID of category") @PathVariable UUID id
+    ) {
         return categoryService.delete(id);
     }
 }
