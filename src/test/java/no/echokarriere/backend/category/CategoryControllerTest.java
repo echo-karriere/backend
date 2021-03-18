11764413c04b238f59dae0f3ef31737eaeb684b9
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-18 20:54:33 +0100

    First pass at controller tests, run Flyway between tests

--- src/test/java/no/echokarriere/backend/category/CategoryControllerTest.java
+++ src/test/java/no/echokarriere/backend/category/CategoryControllerTest.java
@@ -1,7 +1,22 @@
 package no.echokarriere.backend.category;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CategoryControllerTest {
+class CategoryControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void createNewCategory() {
+        var response = restTemplate.postForEntity("/api/categories", new CategoryDTO("REST Title", "Hello", "rest"), Category.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+    }
 }
