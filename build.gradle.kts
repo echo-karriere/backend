302837b3aa2468d7f95352093572993613cd2882
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-21 19:20:23 +0100

    Remove Spring JPA, we're not using it

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-jooq")
     implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-actuator")
-    implementation("org.springframework.boot", "spring-boot-starter-data-jpa")
     implementation("org.springframework.boot", "spring-boot-starter-validation")
     developmentOnly("org.springframework.boot", "spring-boot-devtools")
 
