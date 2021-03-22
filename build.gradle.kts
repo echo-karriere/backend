6b1866164081a6ae2a2c091136d16e3875b5ec93
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-03-22 15:38:10 +0100

    Remove jOOQ from application

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     id("org.springframework.boot")
     id("io.spring.dependency-management")
     id("com.netflix.dgs.codegen")
-    id("nu.studer.jooq")
     id("org.sonarqube")
 }
 
@@ -21,7 +20,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-actuator")
-    implementation("org.springframework.boot", "spring-boot-starter-jooq")
     implementation("org.springframework.boot", "spring-boot-starter-jdbc")
     developmentOnly("org.springframework.boot", "spring-boot-devtools")
 
@@ -35,9 +33,7 @@ dependencies {
     implementation("org.jdbi", "jdbi3-postgres")
 
     implementation("org.flywaydb", "flyway-core", "7.7.0")
-    implementation("org.jooq", "jooq", "3.14.8")
     runtimeOnly("org.postgresql", "postgresql", postgresVersion)
-    jooqGenerator("org.postgresql", "postgresql", postgresVersion)
 
     implementation("com.netflix.graphql.dgs", "graphql-dgs-spring-boot-starter", "3.9.3")
     implementation("com.graphql-java", "graphql-java-extended-scalars", "15.0.0")
@@ -80,45 +76,6 @@ tasks.withType<Test> {
 tasks.withType<com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask> {
     packageName = "no.echokarriere.graphql"
     generateClient = true
-}
-
-jooq {
-    version.set(dependencyManagement.importedProperties["jooq.version"])
-    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
-    configurations {
-        create("main") {
-            jooqConfiguration.apply {
-                logging = org.jooq.meta.jaxb.Logging.WARN
-                jdbc.apply {
-                    driver = "org.postgresql.Driver"
-                    url = (System.getenv("DATABASE_URL") ?: "jdbc:postgresql://localhost:5432/echokarriere")
-                    user = (System.getenv("DATABASE_USER") ?: "karriere")
-                    password = (System.getenv("DATABASE_PASSWORD") ?: "password")
-                }
-                generator.apply {
-                    name = "org.jooq.codegen.DefaultGenerator"
-                    database.apply {
-                        name = "org.jooq.meta.postgres.PostgresDatabase"
-                        inputSchema = "public"
-                    }
-                    generate.apply {
-                        isDeprecated = false
-                        isPojos = false
-                        isJavaTimeTypes = true
-                    }
-                    target.apply {
-                        packageName = "no.echokarriere"
-                    }
-                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
-                }
-            }
-        }
-    }
-}
-
-tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
-    allInputsDeclared.set(true)
-    outputs.cacheIf { true }
 }
 
 tasks.jacocoTestReport {
