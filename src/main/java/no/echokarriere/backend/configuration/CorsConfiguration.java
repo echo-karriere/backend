051599b012bd51181f94109439a240b5ea98782f
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-04 00:19:28 +0200

    Add CORS configuration to allow local webapp to talk to API

---
+++ src/main/java/no/echokarriere/backend/configuration/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package no.echokarriere.backend.configuration;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/graphql").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
