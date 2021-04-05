4917602757f30877b96ce6d932a53bd8362e629e
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-05 20:03:09 +0200

    Add authorities converter to get roles from JWT token

--- src/main/java/no/echokarriere/backend/configuration/SecurityConfiguration.java
+++ src/main/java/no/echokarriere/backend/configuration/SecurityConfiguration.java
@@ -1,19 +1,29 @@
 package no.echokarriere.backend.configuration;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -39,7 +49,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
-                .decoder(jwtDecoder());
+                .decoder(jwtDecoder())
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
     }
 
     CorsConfigurationSource corsConfigurationSource() {
@@ -67,5 +78,50 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
+    }
+
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var customAuthoritiesConverter = new CustomAuthoritiesConverter();
+        var authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(customAuthoritiesConverter);
+        return authenticationConverter;
+    }
+
+    private static class CustomAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+        private static final Map<String, String> CLAIMS_TO_AUTH_PREFIX_MAP = Map.of(
+                "scope", "SCOPE_",
+                "permissions", "PERMISSION_",
+                "https://api.echokarriere.no/roles", "ROLE_",
+                "https://api.echokarriere.no/group", "GROUP_"
+        );
+
+        @Override
+        public Collection<GrantedAuthority> convert(@NotNull Jwt source) {
+            return CLAIMS_TO_AUTH_PREFIX_MAP.entrySet()
+                    .stream()
+                    .map(entry -> getAuthorities(source, entry.getKey(), entry.getValue()))
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList());
+        }
+
+        private Collection<GrantedAuthority> getAuthorities(Jwt jwt, String authorityClaimName, String authorityPrefix) {
+            var authorities = jwt.getClaim(authorityClaimName);
+            if (authorities instanceof String) {
+                if (StringUtils.hasText((String) authorities)) {
+                    var claims = Arrays.asList(((String) authorities).split(" "));
+                    return claims.stream()
+                            .map(claim -> new SimpleGrantedAuthority(authorityPrefix + claim))
+                            .collect(Collectors.toList());
+                } else {
+                    return List.of();
+                }
+            } else if (authorities instanceof Collection) {
+                Collection<String> authoritiesCollection = (Collection<String>) authorities;
+                return authoritiesCollection.stream()
+                        .map(authority -> new SimpleGrantedAuthority(authorityPrefix + authority))
+                        .collect(Collectors.toList());
+            }
+            return List.of();
+        }
     }
 }
