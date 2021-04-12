c9c38f9c08cc17031438b1bc1150f0196b4336f4
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 23:53:08 +0200

    Fix E2E tests

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "src/prisma.service";
 
+import { PrismaService } from "../prisma.service";
 import { CategoryResolver } from "./category.resolver";
 import { CategoryService } from "./category.service";
 
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,6 +16,10 @@ describe("AppController (e2e)", () => {
     await app.init();
   });
 
+  afterAll(() => {
+    void app.close();
+  });
+
   it("/ (GET)", () => {
     return request(app.getHttpServer()).get("/").expect(200).expect("Hello World!");
   });
