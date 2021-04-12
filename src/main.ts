fd78f85afc8e8beac185139e10a9965c36ee00d4
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 22:12:21 +0200

    Move back to Express, the Playground didn't work

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from "@nestjs/core";
-import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create(AppModule);
   await app.listen(8080, "0.0.0.0");
   console.log(`Application is running on: ${await app.getUrl()}`);
 
