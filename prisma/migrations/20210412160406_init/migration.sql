3a8f3ff480c61551cc7863d0a7e722d89d23bf46
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 18:07:12 +0200

    Add and configure Prisma

---
+++ prisma/migrations/20210412160406_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMPTZ(6),
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "homepage" TEXT,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMPTZ(6),
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category.title_unique" ON "Category"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category.slug_unique" ON "Category"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company.name_unique" ON "Company"("name");
