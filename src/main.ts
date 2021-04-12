b66d0af48c62c408369486f81703631860f23a42
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 21:07:01 +0200

    Add GraphQL and configure it

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(8080, "0.0.0.0");
+  console.log(`Application is running on: ${await app.getUrl()}`);
 
   if (module.hot) {
     module.hot.accept();
