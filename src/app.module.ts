cceb3981a488ad38daa21ee8fc876f731df61fe6
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 21:07:01 +0200

    Add GraphQL and configure it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
 import { TerminusModule } from "@nestjs/terminus";
 
 import { AppController } from "./app.controller";
@@ -7,7 +8,14 @@ import { HealthController } from "./health/health.controller";
 import { PrismaService } from "./prisma.service";
 
 @Module({
-  imports: [TerminusModule],
+  imports: [
+    TerminusModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: "schema.gql",
+      installSubscriptionHandlers: true,
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController, HealthController],
   providers: [PrismaService, AppService],
 })
