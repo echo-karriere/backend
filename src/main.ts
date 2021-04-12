3897fc1430ada575d698458436b98d4dcf3645f5
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-12 20:40:24 +0200

    Enable hot reload when developing

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(8080, "0.0.0.0");
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => void app.close());
+  }
 }
 
 void bootstrap();
