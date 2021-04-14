9e54dc5370a716b67980482a613de8ac116e4c27
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-14 18:46:38 +0200

    Add the correct URLs for this CORS config, whoops

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,5 +2,5 @@ import { CorsOptions } from "apollo-server-express";
 
 export const corsConfiguration: CorsOptions = {
   methods: ["POST", "GET"],
-  origin: ["http://localhost:3000", "https://dev.api.echokarriere.no", "https://echokarriere-dev.azurewebsites.net"],
+  origin: ["http://localhost:3000", "https://dev.portal.echokarriere.no", /frontend-[\D\w]*-echokarriere\.vercel\.app/],
 };
