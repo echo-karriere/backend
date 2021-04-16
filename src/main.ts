1784b13738e811c2e7b30af99384a488315029a4
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-16 15:10:14 +0200

    Add helmet to application for some useful headers on requests/responses

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import helmet from "helmet";
 
 import { AppModule } from "./app/app.module";
 import { corsConfiguration } from "./config/cors.config";
@@ -7,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: corsConfiguration });
 
   const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 8080;
+
+  app.use(helmet());
 
   await app.listen(port, "0.0.0.0");
   console.log(`Application is running on: ${await app.getUrl()}`);
