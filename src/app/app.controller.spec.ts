b6d8ce34d4f2a142c7e322b2f16ab92aea2dd7a8
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-16 14:54:06 +0200

    Add test for build information

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -5,6 +5,7 @@ import { AppService } from "./app.service";
 
 describe("AppController", () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -13,11 +14,19 @@ describe("AppController", () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppController);
   });
 
   describe("root", () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe("Hello World!");
+    });
+
+    it("should return build info", () => {
+      const result = { commit: "12345abc", buildDate: new Date() };
+      jest.spyOn(appService, "getBuildInfo").mockImplementation(() => result);
+
+      expect(appController.getBuildInfo()).toBe(result);
     });
   });
 });
