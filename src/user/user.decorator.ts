8ca763b978519bd180cad95737ca7eb377ea6651
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-05-13 11:55:31 +0200

    Fix formatting in new Prettier release

--- src/user/user.decorator.ts
+++ src/user/user.decorator.ts
@@ -5,5 +5,5 @@ import { AzureToken } from "./entities/token.entity";
 
 export const CurrentUser = createParamDecorator((_: unknown, context: ExecutionContext) => {
   const ctx = GqlExecutionContext.create(context);
-  return (ctx.getContext<Record<string, Record<string, string>>>().req.user as unknown) as AzureToken;
+  return ctx.getContext<Record<string, Record<string, string>>>().req.user as unknown as AzureToken;
 });
